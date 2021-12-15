97da244be11c275359436b2d851ce9024919dfbc
Gitesh Dohare <89893323+GiteshDohare@users.noreply.github.com>
2021-12-15 18:51:40 +0530

    updated code to fetch testrail credentials from env

--- index.js
+++ index.js
@@ -1,37 +1,19 @@
+require('dotenv').config();
+const stripAnsi = require('strip-ansi');
 const TestRail = require('testrail');
 
-const dotenv = require('dotenv');
-const fs = require('fs');
-const stripAnsi = require('strip-ansi');
-
-let envFile = null;
-
-try {
-  envFile = fs.readFileSync('.env');
-} catch (error) {
-  console.error("You don't have an .env file!\n", error);
-  process.exit(1);
-}
-
-const config = dotenv.parse(envFile);
-
 const api = new TestRail({
-  host: config.TESTRAIL_URL,
-  user: config.TESTRAIL_USERNAME,
-  password: config.TESTRAIL_PASSWORD,
+  host: process.env.TESTRAIL_URL,
+  user: process.env.TESTRAIL_USERNAME,
+  password: process.env.TESTRAIL_PASSWORD,
 });
 
 class Reporter {
-  
-  constructor(globalConfig, options) {
-    this._globalConfig = globalConfig;
-    this._options = options;
-  }
+  constructor() {}
 
   async createRun(suiteId, tests) {
-    const projectId = this._options.project_id;
-
-    const now = new Date();
+    const projectId = process.env.TESTRAIL_PROJECT_ID;
+    const currentTime = new Date();
 
     const options = {
       year: 'numeric',
@@ -43,26 +25,23 @@ class Reporter {
       hour12: false,
     };
 
-    let message = 'Jest automated test run'
-
-    if (config.TESTRAIL_MESSAGE) {
-      message = config.TESTRAIL_MESSAGE
-    }
-
+    const message = 'Jest Automated Test Run';
     const suite = await api.getSuite(suiteId);
-      
-    const name = `${suite.name} - ${now.toLocaleString(['en-GB'], options)} - (${message})`;
+    const name = `${suite.name} - ${currentTime.toLocaleString(
+        ['en-GB'],
+        options,
+    )} - (${message})`;
 
     const run = await api.addRun(projectId, {
-      suite_id: suiteId,
       name: name,
-      include_all: false,
+      suite_id: suiteId,
       case_ids: Array.from(tests.keys()),
+      include_all: false,
     });
 
     try {
       await api.addResultsForCases(run.id, {
-        results: Array.from(tests).map((value)=>({
+        results: Array.from(tests).map((value) => ({
           case_id: value[0],
           status_id: value[1].status_id,
           comment: value[1].comment,
@@ -70,7 +49,7 @@ class Reporter {
       });
 
       await api.closeRun(run.id);
-    } catch(error) {
+    } catch (error) {
       api.deleteRun(run.id);
 
       console.log(error.message || error);
@@ -79,48 +58,53 @@ class Reporter {
 
   onRunComplete(contexts, results) {
     const specResults = results.testResults;
-    
     const suiteResults = new Map();
 
     for (let j = 0; j < specResults.length; j += 1) {
-      
-      const itResults = specResults[j].testResults;
+      const testResults = specResults[j].testResults;
 
-      for (let i = 0; i < itResults.length; i += 1) {
-        const suiteId = this.getId(itResults[i].ancestorTitles[0]);
+      for (let i = 0; i < testResults.length; i += 1) {
+        const suiteId = this.getId(testResults[i].ancestorTitles[0]);
         if (!suiteId) {
           continue;
         }
-        const testId = this.getId(itResults[i].title);
+
+        const testId = this.getId(testResults[i].title);
+
         if (!testId) {
           continue;
         }
-        const status = itResults[i].status;
+        const status = testResults[i].status;
 
-        let testMap= suiteResults.get(suiteId);
+        let testMap = suiteResults.get(suiteId);
 
         if (!testMap) {
           testMap = new Map();
         }
 
         switch (status) {
-          case 'pending':
-            testMap.set(testId, {
-              status_id: 2,
-              comment: 'Intentionally skipped.',
-            });
-            break;
-          case 'failed':
-            testMap.set(testId, {
-              status_id: 5,
-              comment: stripAnsi(itResults[i].failureMessages[0]),
-            });
-            break;
           case 'passed':
             testMap.set(testId, {
               status_id: 1,
               comment: 'Test passed successfully.',
             });
+
+            break;
+
+          case 'failed':
+            testMap.set(testId, {
+              status_id: 5,
+              comment: stripAnsi(testResults[i].failureMessages[0]),
+            });
+
+            break;
+
+          case 'pending':
+            testMap.set(testId, {
+              status_id: 2,
+              comment: 'Intentionally skipped.',
+            });
+
             break;
           default:
             continue;
@@ -130,18 +114,21 @@ class Reporter {
       }
     }
 
-    for (let [key, value] of suiteResults.entries()) {
+    for (const [key, value] of suiteResults.entries()) {
       this.createRun(key, value);
     }
   }
 
   getId(context) {
+    if (!context) {
+      return;
+    }
     const testRailRegExp = RegExp(/TestRail\[\d{1,}\]/g);
     const matches = context.match(testRailRegExp);
-    if (!matches || matches.length && matches.length > 1) {
+    if (!matches || (matches.length && matches.length > 1)) {
       return null;
     }
-    return + matches[0].match(/\d{1,}/)[0];
+    return +matches[0].match(/\d{1,}/)[0];
   }
 }
 
